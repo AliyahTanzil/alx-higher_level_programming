4658476d2e6c005613926dfc2d41aeae07a054ed
Mohamed Augustin Sesay <sesaymohamedaugustin@gmail.com>
2022-09-21 16:49:44 +0000

    Corrections

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import random
-<<<<<<< HEAD
 number = random.randint(-10, 10)
 if number > 0:
     print("{} is positive".format(number))
@@ -8,7 +7,6 @@ elif number == 0:
     print("{} is zero".format(number))
 elif number < 0:
     print("{} is negative".format(number))
-=======
 num = random.randint(-100, 100)
 if num > 0:
 	print('%s, is positive'%(num))
@@ -16,4 +14,3 @@ elif num < 0:
 	print('%s, is negative'%(num))
 else:
 	print('%s, is zero'%(num))
->>>>>>> 75e423ce099b9f233d5f0c7c2498a090a5af4f83
