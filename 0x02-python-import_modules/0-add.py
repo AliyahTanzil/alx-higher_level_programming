03daa06527fb1d5fc47c7154790d4a32d17b027d
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-09-22 15:52:17 +0000

    Add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
 
-
-a = 1
-
-b = 2
-
-def sum():
-    add()
-total = add(a,b)
-print("{} + {} = {}".format(a,b, total))
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
