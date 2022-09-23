cd14a553eefc5e71572a10522674eb7ac483ce13
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-09-23 13:00:04 +0000

    changes

--- 0x02-python-import_modules/4-hidden_discover.py
+++ 0x02-python-import_modules/4-hidden_discover.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4.pyc
+    """Print all names define by hidden_4 module."""
+    import hidden_4
 
-    names = dir(hidden_4.pyc)
-    for name in names:
+    names = dir(hidden_4)
+    for names in names:
         if name[:2] != "__":
             print(name)
