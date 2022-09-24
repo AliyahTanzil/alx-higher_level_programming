577d3cba625befcb328e1c61e28951f608e70de5
Mohamed Augustin Sesay <sesaymohamedaugustin@gmail.com>
2022-09-24 21:38:07 +0000

    Print List

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
+#!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range(10):
-        my_list.append(i)
-        print("{}".format(i), sep="\n")
-
-print_list_integer(my_list)
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
