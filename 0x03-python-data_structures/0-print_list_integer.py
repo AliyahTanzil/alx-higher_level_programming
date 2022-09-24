7cd2028ca2a03ce1e9367abe7eeb1024f0ee06b1
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-09-24 14:10:00 -0700

    List1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
-=======
->>>>>>> af94b33dd80274ba3336126314e5c4a78bb6496e
+
 def print_list_integer(my_list=[]):
     for i in range(10):
         my_list.append(i)
         print("{}".format(i), sep="\n")
 
+my_list=[]
 print_list_integer(my_list)
